f14bcbba56aae81e5f01abefd7d0ad81fe2c98b1
J0nathan550 <ihudokormov@gmail.com>
2025-01-22 23:38:25 +0100

    Added proper bottom navigation bar and remove system padding

--- app/src/main/java/com/j0nathan550/soundscapes/MainActivity.java
+++ app/src/main/java/com/j0nathan550/soundscapes/MainActivity.java
@@ -10,8 +10,16 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private final SearchFragment searchFragment = new SearchFragment();
+    private final PlaylistFragment playlistFragment = new PlaylistFragment();
+    private final SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,34 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item ->
+        {
+            int id = item.getItemId();
+
+            if(id == R.id.searchMenuItem)
+            {
+                replaceFragment(searchFragment);
+            }
+            else if (id == R.id.playlistMenuItem)
+            {
+                replaceFragment(playlistFragment);
+            }
+            else if (id == R.id.settingsMenuItem)
+            {
+                replaceFragment(settingsFragment);
+            }
+
+            return true;
         });
+
+        bottomNavigationView.setSelectedItemId(R.id.searchMenuItem);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainContent, fragment)
+                .commit();
     }
 }
